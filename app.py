09e8ab18cfd2e7749636eb92b8bdf005ead9497e
dawdawdawd <dadawad@dadada.com>
2023-11-07 22:26:07 +0900

    add gpt4v command

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
+from openai import OpenAI
 
 app = Flask(__name__)
 
@@ -15,4 +16,40 @@ def demo_day():
     delta = target_date - now
     response_text = f'11/3までの残り日数: {delta.days+1} 日'
     
+    return jsonify({"response_type": "in_channel", "text": response_text})
+
+@app.route('/gpt4v', methods=['POST'])
+def gpt4v():
+    response = ""
+    data = request.get_json()
+    inputs = request.values.get('text').strip().split()
+    if len(inputs) != 2:
+        response_text = "invalid input"
+    else:
+        image_url = inputs[0]
+        prompt_text = inputs[1]
+
+        client = OpenAI(
+            # defaults to os.environ.get("OPENAI_API_KEY")
+            api_key="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",
+        )
+
+        response = client.chat.completions.create(
+            model="gpt-4-vision-preview",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": prompt_text},
+                        {
+                            "type": "image_url",
+                            "image_url": image_url,
+                        },
+                    ],
+                }
+            ],
+            max_tokens=500,
+        )
+        response_text = response.choices[0]
+
     return jsonify({"response_type": "in_channel", "text": response_text})
